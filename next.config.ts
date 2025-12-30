a644eeede8d7167214d7a1c46fcacff6347af643
Stephen Amoako <stevekobbi20@gmail.com>
2025-12-30 15:14:34 +0000

    Move all config into the .anv

--- next.config.ts
+++ next.config.ts
@@ -33,10 +33,6 @@ const nextConfig: NextConfig = {
   experimental: {
     serverMinification: false,
   },
-  env: {
-    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-  },
 };
 
 export default nextConfig;
