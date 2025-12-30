0aa8c9c8e80d97ef95a119b1a9ef89f957901acd
Stephen Amoako <stevekobbi20@gmail.com>
2025-12-30 15:17:02 +0000

    ## Error Type Console TypeError
    ## Error Message
    Failed to fetch

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -2,7 +2,10 @@
 
 import { createBrowserClient } from "@supabase/ssr";
 
+// This is not best practice, but is a temporary workaround to ensure the client
+// can connect to Supabase in this development environment.
+// The credentials should be moved to environment variables in a production setup.
 export const supabase = createBrowserClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  "https://tbnqsmstmfpstfblcpfr.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRibnFzbXN0bWZwc3RmYmxjcGZyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE5MDU1NDgsImV4cCI6MjAzNzQ4MTU0OH0.Xb-iA3o-vfkq2ke2_rfp_sCUPw0C_3yN4xPZ7i0kFss",
 );
