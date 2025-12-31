120f47f06f4188970090e8eac69fe1396b3958a6
Stephen Amoako <stevekobbi20@gmail.com>
2025-12-31 11:54:16 +0000

    ## Error Type Console TypeError
    ## Error Message
    Failed to fetch

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -4,6 +4,6 @@ import { createClient } from "@supabase/supabase-js";
 
 // This file is the single source of truth for the client-side Supabase instance.
 export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  "https://rqqvttdajsczwhjvrzve.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJxcXZ0dGRhanNjenFoanZyenZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE0ODE5NTAsImV4cCI6MjAzNzA1Nzk1MH0.3BHtW2E0y1j59-t5-dMtxKqgvyDAtinZlF9oH52c8Fw"
 );
---
+++ src/lib/supabase/server.ts
@@ -0,0 +1,34 @@
+import { createServerClient, type CookieOptions } from '@supabase/ssr'
+import { cookies } from 'next/headers'
+
+export const createClient = (cookieStore: ReturnType<typeof cookies>) => {
+  return createServerClient(
+    "https://rqqvttdajsczwhjvrzve.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJxcXZ0dGRhanNjenFoanZyenZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE0ODE5NTAsImV4cCI6MjAzNzA1Nzk1MH0.3BHtW2E0y1j59-t5-dMtxKqgvyDAtinZlF9oH52c8Fw",
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          try {
+            cookieStore.set({ name, value, ...options })
+          } catch (error) {
+            // The `set` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
+          }
+        },
+        remove(name: string, options: CookieOptions) {
+          try {
+            cookieStore.set({ name, value: '', ...options })
+          } catch (error) {
+            // The `delete` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
+          }
+        },
+      },
+    }
+  )
+}
