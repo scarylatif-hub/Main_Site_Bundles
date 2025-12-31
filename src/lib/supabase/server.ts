0b6fa34572a25fc13ac5bd6c6965ffcd7b5030ef
Stephen Amoako <stevekobbi20@gmail.com>
2025-12-31 17:03:40 +0000

    Try fixing this error: `Console Error: Error fetching user profile: "Inv

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -4,8 +4,8 @@ import { cookies } from 'next/headers'
 
 export const createClient = (cookieStore: ReturnType<typeof cookies>) => {
   return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    "https://ieqrdlbdqilzwibtyyqy.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImllcXJkbGJkcWlsendpYnR5eXF5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE3NDg3NTYsImV4cCI6MjAzNzMyNDc1Nn0.V_g6O5i5j_G-gH4a-qf2X-qUayKy3iN0GvW-iYwX-p0",
     {
       cookies: {
         get(name: string) {
