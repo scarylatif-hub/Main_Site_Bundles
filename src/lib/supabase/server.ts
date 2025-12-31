f2c44ef1d21de8f696890ed40fda7cba3ec501ae
Stephen Amoako <stevekobbi20@gmail.com>
2025-12-31 11:58:44 +0000

    eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -4,7 +4,7 @@ import { cookies } from 'next/headers'
 export const createClient = (cookieStore: ReturnType<typeof cookies>) => {
   return createServerClient(
     "https://ieqrdlbdqilzwibtyyqy.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImllcXJkbGJkcWlsemVpYnR5eXF5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE5MDUzMDMsImV4cCI6MjAzNzQ4MTMwM30.7Q_uNMDcQ_W-B2pI-enirA-HtozYtWp7oYxPjWfD8-8",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImllcXJkbGJkcWlsendpYnR5eXF5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY3MzIwMTUsImV4cCI6MjA4MjMwODAxNX0.vKWlA8t4-vllguDrd7AN7ipYDFGile025bnZ4HSSer8",
     {
       cookies: {
         get(name: string) {
